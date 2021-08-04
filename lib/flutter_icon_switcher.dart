b7fd253eda64889759fb6693eeb90cb4ba63ff55
chentianxin <16chentianxin@gmail.com>
2021-08-04 18:04:30 +0800

    change sdk to 2.12.0

--- lib/flutter_icon_switcher.dart
+++ lib/flutter_icon_switcher.dart
@@ -18,13 +18,13 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterIconSwitcher {
-  static const MethodChannel _channel = MethodChannel('flutter_icon_switcher');
+  static const _channel = MethodChannel('flutter_icon_switcher');
 
   /// Sets the icon.
   /// By running this method, the app will close, and only the user can reopen it.
   /// Rember to set the `AndroidManifest.xml` before
   static Future<void> updateIcon(String configurationName) async {
-    Map<String,dynamic> data = {
+    Map<String, dynamic> data = {
       'name': configurationName,
     };
     await _channel.invokeMethod('updateIcon', data);
@@ -35,5 +35,4 @@ class FlutterIconSwitcher {
   static Future<void> resetIcon() async {
     await _channel.invokeMethod('resetIcon');
   }
-
 }
